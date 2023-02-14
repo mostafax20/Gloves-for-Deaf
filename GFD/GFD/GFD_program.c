8ffe2d2b5a64a4330a5928e066c1d39d6edca125
ziadelbouriny <100305058+ziadelbouriny@users.noreply.github.com>
2023-02-15 01:13:38 +0200

    Add files via upload

--- GFD/GFD/GFD_program.c
+++ GFD/GFD/GFD_program.c
@@ -18,8 +18,8 @@
 #include "ADC_interface.h"
 
            /* HAL */
-//#include "LCD_interface.h"
-//#include "EEPROM_interface.h"
+#include "LCD_interface.h"
+#include "EEPROM_Interface.h"
         /* APPLICATION */
 #include "GDF_interface.h"
 
@@ -34,14 +34,111 @@ void GDF_PinDirectionInit(void)
     DIO_setPinDirection( DIO_PORTA , DIO_PIN4 , DIO_PIN_INPUT );//Channel 4
 
                        /*     LCD PINS      */
-    DIO_setPinDirection(DIO_PORTC,DIO_PIN4,DIO_PIN_OUTPUT);
-    DIO_setPinDirection(DIO_PORTC,DIO_PIN5,DIO_PIN_OUTPUT);
-    DIO_setPinDirection(DIO_PORTC,DIO_PIN6,DIO_PIN_OUTPUT);
-    DIO_setPinDirection(DIO_PORTC,DIO_PIN7,DIO_PIN_OUTPUT);
-    DIO_setPinDirection(DIO_PORTB,DIO_PIN1,DIO_PIN_OUTPUT);
-    DIO_setPinDirection(DIO_PORTB,DIO_PIN2,DIO_PIN_OUTPUT);
-    DIO_setPinDirection(DIO_PORTB,DIO_PIN3,DIO_PIN_OUTPUT);
+     DIO_setPinDirection(DIO_PORTC,DIO_PIN4,DIO_PIN_OUTPUT);
+     DIO_setPinDirection(DIO_PORTC,DIO_PIN5,DIO_PIN_OUTPUT);
+     DIO_setPinDirection(DIO_PORTC,DIO_PIN6,DIO_PIN_OUTPUT);
+     DIO_setPinDirection(DIO_PORTC,DIO_PIN7,DIO_PIN_OUTPUT);
+     DIO_setPinDirection(DIO_PORTB,DIO_PIN1,DIO_PIN_OUTPUT);
+     DIO_setPinDirection(DIO_PORTB,DIO_PIN2,DIO_PIN_OUTPUT);
+     DIO_setPinDirection(DIO_PORTB,DIO_PIN3,DIO_PIN_OUTPUT);
 	 
+}
+
+void savingSentences()
+{
+	EEPROM_Write_Byte(0,'I');
+	EEPROM_Write_Byte(1,' ');
+	EEPROM_Write_Byte(2,'L');
+	EEPROM_Write_Byte(3,'o');
+	EEPROM_Write_Byte(4,'v');
+	EEPROM_Write_Byte(5,'e');
+	EEPROM_Write_Byte(6,' ');
+	EEPROM_Write_Byte(7,'Y');
+	EEPROM_Write_Byte(8,'o');
+	EEPROM_Write_Byte(9,'u');
+	
+	EEPROM_Write_Byte(10,'G');
+	EEPROM_Write_Byte(11,'o');
+	EEPROM_Write_Byte(12,'o');
+	EEPROM_Write_Byte(13,'d');
+	EEPROM_Write_Byte(14,' ');
+	EEPROM_Write_Byte(15,'J');
+	EEPROM_Write_Byte(16,'o');
+	EEPROM_Write_Byte(17,'b');
+	
+	EEPROM_Write_Byte(18,'I');
+	EEPROM_Write_Byte(19,' ');
+	EEPROM_Write_Byte(20,'w');
+	EEPROM_Write_Byte(21,'a');
+	EEPROM_Write_Byte(22,'t');
+	EEPROM_Write_Byte(23,'c');
+	EEPROM_Write_Byte(24,'h');
+	EEPROM_Write_Byte(25,' ');
+	EEPROM_Write_Byte(27,'y');
+	EEPROM_Write_Byte(28,'o');
+	EEPROM_Write_Byte(29,'u');
+	
+	EEPROM_Write_Byte(30,'T');
+	EEPROM_Write_Byte(31,'h');
+	EEPROM_Write_Byte(32,'a');
+	EEPROM_Write_Byte(33,'t');
+	EEPROM_Write_Byte(34,' ');
+	EEPROM_Write_Byte(35,'i');
+	EEPROM_Write_Byte(36,'s');
+	EEPROM_Write_Byte(37,' ');
+	EEPROM_Write_Byte(38,'f');
+	EEPROM_Write_Byte(39,'a');
+	EEPROM_Write_Byte(40,'n');
+	EEPROM_Write_Byte(41,'t');
+	EEPROM_Write_Byte(42,'a');
+	EEPROM_Write_Byte(43,'s');
+	EEPROM_Write_Byte(44,'t');
+	EEPROM_Write_Byte(45,'i');
+	EEPROM_Write_Byte(46,'c');
+	
+	EEPROM_Write_Byte(47,'I');
+	EEPROM_Write_Byte(48,' ');
+	EEPROM_Write_Byte(49,'w');
+	EEPROM_Write_Byte(50,'i');
+	EEPROM_Write_Byte(51,'s');
+	EEPROM_Write_Byte(52,'h');
+	EEPROM_Write_Byte(53,' ');
+	EEPROM_Write_Byte(54,'y');
+	EEPROM_Write_Byte(55,'o');
+	EEPROM_Write_Byte(56,'u');
+	EEPROM_Write_Byte(57,' ');
+	EEPROM_Write_Byte(58,'g');
+	EEPROM_Write_Byte(59,'o');
+	EEPROM_Write_Byte(60,'o');
+	EEPROM_Write_Byte(61,'d');
+	EEPROM_Write_Byte(62,' ');
+	EEPROM_Write_Byte(63,'l');
+	EEPROM_Write_Byte(64,'i');
+	EEPROM_Write_Byte(65,'f');
+	EEPROM_Write_Byte(67,'e');
+	
+	EEPROM_Write_Byte(68,'I');
+	EEPROM_Write_Byte(69,' ');
+	EEPROM_Write_Byte(70,'r');
+	EEPROM_Write_Byte(71,'e');
+	EEPROM_Write_Byte(72,'a');
+	EEPROM_Write_Byte(73,'l');
+	EEPROM_Write_Byte(74,'l');
+	EEPROM_Write_Byte(75,'y');
+	EEPROM_Write_Byte(76,' ');
+	EEPROM_Write_Byte(78,'l');
+	EEPROM_Write_Byte(79,'o');
+	EEPROM_Write_Byte(80,'v');
+	EEPROM_Write_Byte(81,'e');
+	EEPROM_Write_Byte(82,' ');
+	EEPROM_Write_Byte(83,'y');
+	EEPROM_Write_Byte(84,'o');
+	EEPROM_Write_Byte(85,'u');
+	
+	EEPROM_Write_Byte(86,'Y');
+	EEPROM_Write_Byte(87,'o');
+	EEPROM_Write_Byte(88,'u');
+	
 }
 
 void GDF_MainCode(void)
@@ -78,62 +175,68 @@ void GDF_MainCode(void)
 		ADC_getDigitalValue( ADC_SINGLE_ENDED_CHANNEL_4 , &Digital_Little_Finger );
 		Analog_Little_Finger = (( (u32)Digital_Little_Finger * 5000UL )/1024 );
 		
-		if (  (Analog_Thumb < 625  ) && (Analog_Index_Finger < 625  ) && (Analog_Middle_Finger < 625 )  &&  ( Analog_Ring_Finger  < 625 )  &&  ( Analog_Little_Finger < 625 )   )//All fingers Flat
+		if (  (Analog_Thumb < ) && (Analog_Index_Finger < ) && (Analog_Middle_Finger < )  &&  ( Analog_Ring_Finger  < )  &&  ( Analog_Little_Finger < )   )
 		{  
 			 _delay_ms(1000);
 			//Clear LCD 
-			//SENTENCE FROM EEPROM CORRESPONDING TO THE RELATIVE VALUES TO BE PRINTED ON THE LCD " I wish you a happy life " 
-			// LCD SEND CHAR
+			u8 st[10];
+			st[9] = '\0';
+			for(u8 i = 0; i <= 9; i++)
+			{
+				st[i] = EEPROM_Read_Byte(i);
+			};
+			
+			LCD_display(st);
 			
 		}
-		else if (  (Analog_Thumb < 625 ) && (Analog_Index_Finger < 625 ) && ( 3125 < Analog_Middle_Finger < 5000 )  &&  ( 3125 < Analog_Ring_Finger  < 5000 )  &&  ( Analog_Little_Finger < 625 )   )//All fingers flat except middle and ring are totally bended
+		else if (  (Analog_Thumb < ) && (Analog_Index_Finger < ) && (Analog_Middle_Finger < )  &&  ( Analog_Ring_Finger  < )  &&  ( Analog_Little_Finger < )   )
 		{
 			_delay_ms(1000);
 			//Clear LCD
-			//SENTENCE FROM EEPROM CORRESPONDING TO THE RELATIVE VALUES TO BE PRINTED ON THE LCD "I love you "
+			//SENTENCE FROM EEPROM CORRESPONDING TO THE RELATIVE VALUES TO BE PRINTED ON THE LCD
 			// LCD SEND CHAR
 			
 
 		}
-		else if (  ( 3125 < Analog_Thumb < 5000 ) && (Analog_Index_Finger < 625 ) && ( 3125 < Analog_Middle_Finger < 5000 )  &&  ( 3125 < Analog_Ring_Finger  < 5000 )  &&  ( Analog_Little_Finger < 625 )   )////All fingers flat except middle, ring and thumb are totally bended
+		else if (  (Analog_Thumb < ) && (Analog_Index_Finger < ) && (Analog_Middle_Finger < )  &&  ( Analog_Ring_Finger  < )  &&  ( Analog_Little_Finger < )   )
 		{   
 			
 			 _delay_ms(1000);
 			//Clear LCD
-			//SENTENCE FROM EEPROM CORRESPONDING TO THE RELATIVE VALUES TO BE PRINTED ON THE LCD "This is fantastic "
+			//SENTENCE FROM EEPROM CORRESPONDING TO THE RELATIVE VALUES TO BE PRINTED ON THE LCD
 			// LCD SEND CHAR
 		}
 		
-		else if (  (Analog_Thumb < 625 ) && (Analog_Index_Finger < 625 ) && (Analog_Middle_Finger < 625 )  &&  ( 3125 < Analog_Ring_Finger  < 5000 )  &&  ( Analog_Little_Finger < 625 )   )////All fingers flat except ring is totally bended
+		else if (  (Analog_Thumb < ) && (Analog_Index_Finger < ) && (Analog_Middle_Finger < )  &&  ( Analog_Ring_Finger  < )  &&  ( Analog_Little_Finger < )   )
 		{   
 			
 			 _delay_ms(1000);
 			 //Clear LCD
-			 //SENTENCE FROM EEPROM CORRESPONDING TO THE RELATIVE VALUES TO BE PRINTED ON THE LCD" Really i love you"
+			 //SENTENCE FROM EEPROM CORRESPONDING TO THE RELATIVE VALUES TO BE PRINTED ON THE LCD
 			 // LCD SEND CHAR
 		}
-		else if (  ( 3125 < Analog_Thumb < 5000 ) && ( 1125 < Analog_Index_Finger < 3125 ) && ( 3125 < Analog_Middle_Finger < 5000 )  &&  ( 3125 < Analog_Ring_Finger  < 5000 )  &&  ( 3125 < Analog_Little_Finger < 5000 )   )//All fingers are bended totally except index is bended by 45 degrees 
+		else if (  (Analog_Thumb < ) && (Analog_Index_Finger < ) && (Analog_Middle_Finger < )  &&  ( Analog_Ring_Finger  < )  &&  ( Analog_Little_Finger < )   )
 		{   
 			
 			 _delay_ms(1000);
 			 //Clear LCD
-			 //SENTENCE FROM EEPROM CORRESPONDING TO THE RELATIVE VALUES TO BE PRINTED ON THE LCD" You "
+			 //SENTENCE FROM EEPROM CORRESPONDING TO THE RELATIVE VALUES TO BE PRINTED ON THE LCD
 			 // LCD SEND CHAR
 		}
-		else if (  (Analog_Thumb < 625 ) && ( 3125 < Analog_Index_Finger < 5000 ) && ( 3125 < Analog_Middle_Finger < 5000 )  &&  ( 3125 < Analog_Ring_Finger  < 5000 )  &&  ( 3125 < Analog_Little_Finger < 5000 )   )//All fingers are bended except thumb is flat
+		else if (  (Analog_Thumb < ) && (Analog_Index_Finger < ) && (Analog_Middle_Finger < )  &&  ( Analog_Ring_Finger  < )  &&  ( Analog_Little_Finger < )   )
 		{   
 			
 			 _delay_ms(1000);
 			 //Clear LCD
-			 //SENTENCE FROM EEPROM CORRESPONDING TO THE RELATIVE VALUES TO BE PRINTED ON THE LCD" Good job "
+			 //SENTENCE FROM EEPROM CORRESPONDING TO THE RELATIVE VALUES TO BE PRINTED ON THE LCD
 			 // LCD SEND CHAR
 		}
-		else if (  ( 3125 < Analog_Thumb < 5000 ) && ( 1125 < Analog_Index_Finger < 3125 ) && ( 1125 < Analog_Middle_Finger < 3125 )  &&  ( 3125 < Analog_Ring_Finger  < 5000 )  &&  ( 3125 < Analog_Little_Finger < 5000 )   )//All fingers are bended totally except middle and index are bended by 45 degrees
+		else if (  (Analog_Thumb < ) && (Analog_Index_Finger < ) && (Analog_Middle_Finger < )  &&  ( Analog_Ring_Finger  < )  &&  ( Analog_Little_Finger < )   )
 		{   
 			
 			 _delay_ms(1000);
 			//Clear LCD
-			//SENTENCE FROM EEPROM CORRESPONDING TO THE RELATIVE VALUES TO BE PRINTED ON THE LCD" I watch you "
+			//SENTENCE FROM EEPROM CORRESPONDING TO THE RELATIVE VALUES TO BE PRINTED ON THE LCD
 			// LCD SEND CHAR
 		}
 		

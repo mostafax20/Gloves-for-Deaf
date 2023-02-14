2efadeb4d341dfef93738a3fe9edd3f3eb6b1666
MostafaMohamedAbelAzeem <4ikawy.mo7med@gmail.com>
2023-02-14 21:58:40 +0200

    Adding values to be compared with sensors values

--- GFD/GFD/GFD_program.c
+++ GFD/GFD/GFD_program.c
@@ -19,6 +19,7 @@
 
            /* HAL */
 //#include "LCD_interface.h"
+//#include "EEPROM_interface.h"
         /* APPLICATION */
 #include "GDF_interface.h"
 
@@ -33,13 +34,13 @@ void GDF_PinDirectionInit(void)
     DIO_setPinDirection( DIO_PORTA , DIO_PIN4 , DIO_PIN_INPUT );//Channel 4
 
                        /*     LCD PINS      */
-     DIO_setPinDirection(DIO_PORTC,DIO_PIN4,DIO_PIN_OUTPUT);
-     DIO_setPinDirection(DIO_PORTC,DIO_PIN5,DIO_PIN_OUTPUT);
-     DIO_setPinDirection(DIO_PORTC,DIO_PIN6,DIO_PIN_OUTPUT);
-     DIO_setPinDirection(DIO_PORTC,DIO_PIN7,DIO_PIN_OUTPUT);
-     DIO_setPinDirection(DIO_PORTB,DIO_PIN1,DIO_PIN_OUTPUT);
-     DIO_setPinDirection(DIO_PORTB,DIO_PIN2,DIO_PIN_OUTPUT);
-     DIO_setPinDirection(DIO_PORTB,DIO_PIN3,DIO_PIN_OUTPUT);
+    DIO_setPinDirection(DIO_PORTC,DIO_PIN4,DIO_PIN_OUTPUT);
+    DIO_setPinDirection(DIO_PORTC,DIO_PIN5,DIO_PIN_OUTPUT);
+    DIO_setPinDirection(DIO_PORTC,DIO_PIN6,DIO_PIN_OUTPUT);
+    DIO_setPinDirection(DIO_PORTC,DIO_PIN7,DIO_PIN_OUTPUT);
+    DIO_setPinDirection(DIO_PORTB,DIO_PIN1,DIO_PIN_OUTPUT);
+    DIO_setPinDirection(DIO_PORTB,DIO_PIN2,DIO_PIN_OUTPUT);
+    DIO_setPinDirection(DIO_PORTB,DIO_PIN3,DIO_PIN_OUTPUT);
 	 
 }
 
@@ -77,62 +78,62 @@ void GDF_MainCode(void)
 		ADC_getDigitalValue( ADC_SINGLE_ENDED_CHANNEL_4 , &Digital_Little_Finger );
 		Analog_Little_Finger = (( (u32)Digital_Little_Finger * 5000UL )/1024 );
 		
-		if (  (Analog_Thumb < ) && (Analog_Index_Finger < ) && (Analog_Middle_Finger < )  &&  ( Analog_Ring_Finger  < )  &&  ( Analog_Little_Finger < )   )
+		if (  (Analog_Thumb < 625  ) && (Analog_Index_Finger < 625  ) && (Analog_Middle_Finger < 625 )  &&  ( Analog_Ring_Finger  < 625 )  &&  ( Analog_Little_Finger < 625 )   )//All fingers Flat
 		{  
 			 _delay_ms(1000);
 			//Clear LCD 
-			//SENTENCE FROM EEPROM CORRESPONDING TO THE RELATIVE VALUES TO BE PRINTED ON THE LCD 
+			//SENTENCE FROM EEPROM CORRESPONDING TO THE RELATIVE VALUES TO BE PRINTED ON THE LCD " I wish you a happy life " 
 			// LCD SEND CHAR
 			
 		}
-		else if (  (Analog_Thumb < ) && (Analog_Index_Finger < ) && (Analog_Middle_Finger < )  &&  ( Analog_Ring_Finger  < )  &&  ( Analog_Little_Finger < )   )
+		else if (  (Analog_Thumb < 625 ) && (Analog_Index_Finger < 625 ) && ( 3125 < Analog_Middle_Finger < 5000 )  &&  ( 3125 < Analog_Ring_Finger  < 5000 )  &&  ( Analog_Little_Finger < 625 )   )//All fingers flat except middle and ring are totally bended
 		{
 			_delay_ms(1000);
 			//Clear LCD
-			//SENTENCE FROM EEPROM CORRESPONDING TO THE RELATIVE VALUES TO BE PRINTED ON THE LCD
+			//SENTENCE FROM EEPROM CORRESPONDING TO THE RELATIVE VALUES TO BE PRINTED ON THE LCD "I love you "
 			// LCD SEND CHAR
 			
 
 		}
-		else if (  (Analog_Thumb < ) && (Analog_Index_Finger < ) && (Analog_Middle_Finger < )  &&  ( Analog_Ring_Finger  < )  &&  ( Analog_Little_Finger < )   )
+		else if (  ( 3125 < Analog_Thumb < 5000 ) && (Analog_Index_Finger < 625 ) && ( 3125 < Analog_Middle_Finger < 5000 )  &&  ( 3125 < Analog_Ring_Finger  < 5000 )  &&  ( Analog_Little_Finger < 625 )   )////All fingers flat except middle, ring and thumb are totally bended
 		{   
 			
 			 _delay_ms(1000);
 			//Clear LCD
-			//SENTENCE FROM EEPROM CORRESPONDING TO THE RELATIVE VALUES TO BE PRINTED ON THE LCD
+			//SENTENCE FROM EEPROM CORRESPONDING TO THE RELATIVE VALUES TO BE PRINTED ON THE LCD "This is fantastic "
 			// LCD SEND CHAR
 		}
 		
-		else if (  (Analog_Thumb < ) && (Analog_Index_Finger < ) && (Analog_Middle_Finger < )  &&  ( Analog_Ring_Finger  < )  &&  ( Analog_Little_Finger < )   )
+		else if (  (Analog_Thumb < 625 ) && (Analog_Index_Finger < 625 ) && (Analog_Middle_Finger < 625 )  &&  ( 3125 < Analog_Ring_Finger  < 5000 )  &&  ( Analog_Little_Finger < 625 )   )////All fingers flat except ring is totally bended
 		{   
 			
 			 _delay_ms(1000);
 			 //Clear LCD
-			 //SENTENCE FROM EEPROM CORRESPONDING TO THE RELATIVE VALUES TO BE PRINTED ON THE LCD
+			 //SENTENCE FROM EEPROM CORRESPONDING TO THE RELATIVE VALUES TO BE PRINTED ON THE LCD" Really i love you"
 			 // LCD SEND CHAR
 		}
-		else if (  (Analog_Thumb < ) && (Analog_Index_Finger < ) && (Analog_Middle_Finger < )  &&  ( Analog_Ring_Finger  < )  &&  ( Analog_Little_Finger < )   )
+		else if (  ( 3125 < Analog_Thumb < 5000 ) && ( 1125 < Analog_Index_Finger < 3125 ) && ( 3125 < Analog_Middle_Finger < 5000 )  &&  ( 3125 < Analog_Ring_Finger  < 5000 )  &&  ( 3125 < Analog_Little_Finger < 5000 )   )//All fingers are bended totally except index is bended by 45 degrees 
 		{   
 			
 			 _delay_ms(1000);
 			 //Clear LCD
-			 //SENTENCE FROM EEPROM CORRESPONDING TO THE RELATIVE VALUES TO BE PRINTED ON THE LCD
+			 //SENTENCE FROM EEPROM CORRESPONDING TO THE RELATIVE VALUES TO BE PRINTED ON THE LCD" You "
 			 // LCD SEND CHAR
 		}
-		else if (  (Analog_Thumb < ) && (Analog_Index_Finger < ) && (Analog_Middle_Finger < )  &&  ( Analog_Ring_Finger  < )  &&  ( Analog_Little_Finger < )   )
+		else if (  (Analog_Thumb < 625 ) && ( 3125 < Analog_Index_Finger < 5000 ) && ( 3125 < Analog_Middle_Finger < 5000 )  &&  ( 3125 < Analog_Ring_Finger  < 5000 )  &&  ( 3125 < Analog_Little_Finger < 5000 )   )//All fingers are bended except thumb is flat
 		{   
 			
 			 _delay_ms(1000);
 			 //Clear LCD
-			 //SENTENCE FROM EEPROM CORRESPONDING TO THE RELATIVE VALUES TO BE PRINTED ON THE LCD
+			 //SENTENCE FROM EEPROM CORRESPONDING TO THE RELATIVE VALUES TO BE PRINTED ON THE LCD" Good job "
 			 // LCD SEND CHAR
 		}
-		else if (  (Analog_Thumb < ) && (Analog_Index_Finger < ) && (Analog_Middle_Finger < )  &&  ( Analog_Ring_Finger  < )  &&  ( Analog_Little_Finger < )   )
+		else if (  ( 3125 < Analog_Thumb < 5000 ) && ( 1125 < Analog_Index_Finger < 3125 ) && ( 1125 < Analog_Middle_Finger < 3125 )  &&  ( 3125 < Analog_Ring_Finger  < 5000 )  &&  ( 3125 < Analog_Little_Finger < 5000 )   )//All fingers are bended totally except middle and index are bended by 45 degrees
 		{   
 			
 			 _delay_ms(1000);
 			//Clear LCD
-			//SENTENCE FROM EEPROM CORRESPONDING TO THE RELATIVE VALUES TO BE PRINTED ON THE LCD
+			//SENTENCE FROM EEPROM CORRESPONDING TO THE RELATIVE VALUES TO BE PRINTED ON THE LCD" I watch you "
 			// LCD SEND CHAR
 		}
 		
--- GFD/GFD/main.c
+++ GFD/GFD/main.c
@@ -4,11 +4,15 @@
  * Created: 2/13/2023 2:36:46 PM
  * Author : Mostafa
  */ 
-
+  
+          /* APPLICATION */
+ #include "GDF_interface.h"
 
 
 int main(void)
 {
+	GDF_PinDirectionInit();
+	GDF_MainCode();
     /* Replace with your application code */
     while (1) 
     {
